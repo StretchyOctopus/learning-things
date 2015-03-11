76b488a635bdbcd78768af76b979e6a27f744163
Sarah Johnston <sarahjohnston502@gmail.com>
2015-03-11 17:11:53 +0000

    First commit!

---
+++ JavaCalculator/src/calculatorProject/Calculator.java
@@ -0,0 +1,14 @@
+package calculatorProject;
+
+public class Calculator {
+	
+	public static void main (String[] args)
+	{
+		System.out.println("Test");
+	}
+	
+	/*public static int add (int a, int b)
+	{
+		return a + b;
+	}*/
+}
